c78b5c4ddaa2b73ab3d94bf6a0876280a0962915
Daniel Jiménez Rivera <dani@internxt.com>
2025-07-28 10:24:01 +0200

    Add publish npm

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,4 +1,4 @@
-import { logger } from './core/logger/logger';
+import { logger, TLoggerBody } from './core/logger/logger';
 import { setupElectronLog } from './core/logger/setup-electron-log';
 
-export { logger, setupElectronLog };
+export { logger, setupElectronLog, TLoggerBody };
