59155b50daf97bcdc72a5b91a028ccc293e04902
Daniel Jiménez Rivera <dani@internxt.com>
2025-07-29 18:41:36 +0200

    Add eslint plugin sonar (#3)

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,7 +2,7 @@ import { logger, TLoggerBody } from './core/logger/logger';
 import { setupElectronLog } from './core/logger/setup-electron-log';
 import { IssuesModule, BackupsIssue, GeneralIssue, Issue, SyncIssue } from './features/issues/issues.module';
 import { createDriveServerWipModule } from './infra/drive-server-wip/create-drive-server-wip-module';
-import { paths } from './infra/drive-server-wip/schema';
+import { paths, components } from './infra/drive-server-wip/schema';
 
 export {
   logger,
@@ -10,6 +10,7 @@ export {
   TLoggerBody,
   createDriveServerWipModule,
   paths,
+  components,
   IssuesModule,
   SyncIssue,
   BackupsIssue,
