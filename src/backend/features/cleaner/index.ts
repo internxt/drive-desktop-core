6fef6e7b747fa9910adce464de1761dfc9d5250c
Esteban Galvis <egalvis@shokworks.io>
2025-09-22 08:02:04 -0500

    refactor: remove unused import for isFirefoxProfileDirectory from cleaner module

--- src/backend/features/cleaner/index.ts
+++ src/backend/features/cleaner/index.ts
@@ -1,6 +1,5 @@
 import { getDiskSpace } from './utils/get-disk-space';
 import { isInternxtRelated } from './utils/is-file-internxt-related';
-import { isFirefoxProfileDirectory } from './utils/is-firefox-profile-directory';
 import { getAllItemsToDelete, getSelectedItemsForSection } from './utils/selection-utils';
 
-export { getDiskSpace, isFirefoxProfileDirectory, isInternxtRelated, getAllItemsToDelete, getSelectedItemsForSection };
+export { getDiskSpace, isInternxtRelated, getAllItemsToDelete, getSelectedItemsForSection };
