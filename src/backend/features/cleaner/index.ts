f05f8e942700ccfe13e61d4c36f11761410e302d
Esteban Galvis <egalvis@shokworks.io>
2025-09-22 06:25:50 -0500

    refactor: make merge short

--- src/backend/features/cleaner/index.ts
+++ src/backend/features/cleaner/index.ts
@@ -2,9 +2,6 @@ import { getDiskSpace } from './utils/get-disk-space';
 import { isFirefoxProfileDirectory } from './utils/is-firefox-profile-directory';
 import { isInternxtRelated } from './utils/is-file-internxt-related';
 import { getAllItemsToDelete, getSelectedItemsForSection } from './utils/selection-utils';
-import { scanDirectory } from './scan/scan-directory';
-import { scanSubDirectory } from './scan/scan-subdirectory';
-import { scanSingleFile } from './scan/scan-single-file';
 
 export {
     getDiskSpace,
@@ -12,7 +9,4 @@ export {
     isInternxtRelated,
     getAllItemsToDelete,
     getSelectedItemsForSection,
-    scanDirectory,
-    scanSubDirectory,
-    scanSingleFile
 };
