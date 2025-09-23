1a0adb08b2b5c3448872576988a606461e63ce7b
Esteban Galvis <egalvis@shokworks.io>
2025-09-23 13:47:23 -0500

    feat: export additional scanning functions for improved functionality

--- src/backend/features/cleaner/index.ts
+++ src/backend/features/cleaner/index.ts
@@ -1,5 +1,18 @@
+import { processDirent } from './scan/process-dirent';
+import { scanDirectory } from './scan/scan-directory';
+import { scanSingleFile } from './scan/scan-single-file';
+import { scanSubDirectory } from './scan/scan-subdirectory';
 import { getDiskSpace } from './utils/get-disk-space';
 import { isInternxtRelated } from './utils/is-file-internxt-related';
 import { getAllItemsToDelete, getSelectedItemsForSection } from './utils/selection-utils';
 
-export { getDiskSpace, isInternxtRelated, getAllItemsToDelete, getSelectedItemsForSection };
+export {
+  getDiskSpace,
+  isInternxtRelated,
+  getAllItemsToDelete,
+  getSelectedItemsForSection,
+  processDirent,
+  scanDirectory,
+  scanSingleFile,
+  scanSubDirectory,
+};
