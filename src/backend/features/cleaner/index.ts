9881842b58f0a1a35cc54191b653528a37c2f57e
Esteban Galvis <egalvis@shokworks.io>
2025-09-24 10:13:33 -0500

    refactor: simplify export statement in cleaner module

--- src/backend/features/cleaner/index.ts
+++ src/backend/features/cleaner/index.ts
@@ -5,12 +5,4 @@ import { getDiskSpace } from './utils/get-disk-space';
 import { isInternxtRelated } from './utils/is-file-internxt-related';
 import { getAllItemsToDelete, getSelectedItemsForSection } from './utils/selection-utils';
 
-export {
-  getDiskSpace,
-  isInternxtRelated,
-  getAllItemsToDelete,
-  getSelectedItemsForSection,
-  processDirent,
-  scanDirectory,
-  scanSingleFile,
-};
+export { getDiskSpace, isInternxtRelated, getAllItemsToDelete, getSelectedItemsForSection, processDirent, scanDirectory, scanSingleFile };
