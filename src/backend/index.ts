369608626a25c8e27eb8b19ef41f24bdbe3c7ef5
Daniel Jiménez Rivera <dani@internxt.com>
2025-09-26 12:35:56 +0200

    Remove drive-server-wip (#17)

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,14 +2,10 @@ import { logger, TLoggerBody } from './core/logger/logger';
 import { setupElectronLog } from './core/logger/setup-electron-log';
 import { IssuesModule, BackupsIssue, GeneralIssue, Issue, SyncIssue } from './features/issues/issues.module';
 import { UserAvailableProducts, getUserAvailableProducts } from './features/payments';
-import { createDriveServerWipModule } from './infra/drive-server-wip/create-drive-server-wip-module';
-import { paths, components } from './infra/drive-server-wip/schema';
 
 export {
   logger,
   TLoggerBody,
-  paths,
-  components,
   IssuesModule,
   SyncIssue,
   BackupsIssue,
@@ -17,6 +13,5 @@ export {
   Issue,
   UserAvailableProducts,
   setupElectronLog,
-  createDriveServerWipModule,
   getUserAvailableProducts,
 };
