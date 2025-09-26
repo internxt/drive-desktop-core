9f09c08bb964a87696edaa5e5a43dc1738eebcbe
Daniel Jiménez Rivera <dani@internxt.com>
2025-09-26 12:40:59 +0200

    Commit

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,17 +1,5 @@
 import { logger, TLoggerBody } from './core/logger/logger';
 import { setupElectronLog } from './core/logger/setup-electron-log';
-import { IssuesModule, BackupsIssue, GeneralIssue, Issue, SyncIssue } from './features/issues/issues.module';
 import { UserAvailableProducts, getUserAvailableProducts } from './features/payments';
 
-export {
-  logger,
-  TLoggerBody,
-  IssuesModule,
-  SyncIssue,
-  BackupsIssue,
-  GeneralIssue,
-  Issue,
-  UserAvailableProducts,
-  setupElectronLog,
-  getUserAvailableProducts,
-};
+export { logger, TLoggerBody, UserAvailableProducts, setupElectronLog, getUserAvailableProducts };
