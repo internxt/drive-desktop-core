152ec0e31673709d5624e50a46caa9f9024c720e
Esteban Galvis <egalvis@shokworks.io>
2025-09-29 09:30:34 -0500

    Feat/add common scan filters (#15)

--- src/backend/features/cleaner/scan/scan-directory.ts
+++ src/backend/features/cleaner/scan/scan-directory.ts
@@ -1,33 +1,35 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { stat, readdir } from 'fs/promises';
+import { join } from 'path/posix';
 
 import { logger } from '@/backend/core/logger/logger';
 
-import { CleanableItem } from '../types/cleaner.types';
+import { CleanableItem, CleanerContext } from '../types/cleaner.types';
 import { isInternxtRelated } from '../utils/is-file-internxt-related';
 import { processDirent } from './process-dirent';
 
-type ScanDirectoryProps = {
+type Props = {
+  ctx: CleanerContext;
   dirPath: string;
-  customFileFilter?: ({ fileName }: { fileName: string }) => boolean;
+  customFileFilter?: ({ ctx, fileName }: { ctx: CleanerContext; fileName: string }) => boolean;
   customDirectoryFilter?: ({ folderName }: { folderName: string }) => boolean;
 };
 
-export async function scanDirectory({ dirPath, customFileFilter, customDirectoryFilter }: ScanDirectoryProps) {
+export async function scanDirectory({ ctx, dirPath, customFileFilter, customDirectoryFilter }: Props) {
   try {
-    const stat = await fs.stat(dirPath);
-    if (!stat.isDirectory()) {
+    const folderStats = await stat(dirPath);
+    if (!folderStats.isDirectory()) {
       return [];
     }
 
-    const dirents = await fs.readdir(dirPath, { withFileTypes: true });
+    const dirents = await readdir(dirPath, { withFileTypes: true });
     const items: CleanableItem[] = [];
 
     for (const dirent of dirents) {
-      const fullPath = path.join(dirPath, dirent.name);
+      const fullPath = join(dirPath, dirent.name);
       if (isInternxtRelated({ name: fullPath })) continue;
 
       const cleanableItems = await processDirent({
+        ctx,
         entry: dirent,
         fullPath,
         customFileFilter,
