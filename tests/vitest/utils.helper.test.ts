df5d8a8b487a2d201d2aa394683c7d7f521fee36
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-01 14:15:17 +0200

    Update utils.helper.test.ts

--- tests/vitest/utils.helper.test.ts
+++ tests/vitest/utils.helper.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { DeepPartial } from 'ts-essentials';
 import { MockedFunction, MockInstance } from 'vitest';
