41c67a8fb7a52084f0e1928050095416fe3fd7e6
Esteban Galvis <egalvis@shokworks.io>
2025-10-01 10:45:16 -0500

    Refactor: remove stopCleanup import and usage from CleanerModule

--- src/backend/features/cleaner/cleaner.module.ts
+++ src/backend/features/cleaner/cleaner.module.ts
@@ -2,7 +2,7 @@ import { processDirent } from './scan/process-dirent';
 import { scanDirectory } from './scan/scan-directory';
 import { scanSingleFile } from './scan/scan-single-file';
 import { scanSubDirectory } from './scan/scan-subdirectory';
-import { startCleanup, stopCleanup } from './services/cleanup-actions';
+import { startCleanup } from './services/start-cleanup';
 import { getDiskSpace } from './utils/get-disk-space';
 import { isInternxtRelated } from './utils/is-file-internxt-related';
 import { getAllItemsToDelete, getSelectedItemsForSection } from './utils/selection-utils';
@@ -17,5 +17,4 @@ export const CleanerModule = {
   scanSingleFile,
   scanSubDirectory,
   startCleanup,
-  stopCleanup,
 };
