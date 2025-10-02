25b4e6cb642c61e968257b9ecd1d7b2896abe01a
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-02 17:42:46 +0200

    Update throw-wrapper.ts

--- src/backend/core/utils/throw-wrapper.ts
+++ src/backend/core/utils/throw-wrapper.ts
@@ -1,6 +1,6 @@
 type Result<T> = { data: T; error: undefined } | { data: undefined; error: Error };
 
-export function throwWrapper<Args extends any[], T>(fn: (...args: Args) => Promise<Result<T>>) {
+export function throwWrapper<Args extends unknown[], T>(fn: (...args: Args) => Promise<Result<T>>) {
   return async (...args: Args): Promise<T> => {
     const { data, error } = await fn(...args);
     if (error) throw error;
