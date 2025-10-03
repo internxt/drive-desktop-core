89bbc4d91f5c8343965ae5f633fb646fbfaeb363
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-03 16:30:10 +0200

    Simplify

--- src/frontend/features/cleaner/service/calculate-chart-segments.ts
+++ src/frontend/features/cleaner/service/calculate-chart-segments.ts
@@ -1,6 +1,7 @@
 import { CleanerSection, CleanerViewModel } from '@/backend/features/cleaner/types/cleaner.types';
 
 import { SectionConfig } from '../cleaner.types';
+import { calculateSectionSize } from './calculate-section-size';
 import { getSectionStats } from './get-section-stats';
 
 type calculateChartSegmentsProps<T extends Record<string, CleanerSection>> = {
@@ -10,25 +11,6 @@ type calculateChartSegmentsProps<T extends Record<string, CleanerSection>> = {
   getSectionSelectionStats: (sectionKey: string, report: T) => ReturnType<typeof getSectionStats>;
   sectionConfig: SectionConfig;
 };
-
-function calculateExceptionsSize(section: CleanerSection, exceptionPaths: string[]): number {
-  let size = 0;
-  for (const exceptionPath of exceptionPaths) {
-    const item = section.items.find((item) => item.fullPath === exceptionPath);
-    if (item) {
-      size += item.sizeInBytes;
-    }
-  }
-  return size;
-}
-
-function calculateSectionSelectedSize(section: CleanerSection, sectionViewModel: CleanerViewModel[string]): number {
-  if (sectionViewModel.selectedAll) {
-    const exceptionsSize = calculateExceptionsSize(section, sectionViewModel.exceptions);
-    return section.totalSizeInBytes - exceptionsSize;
-  }
-  return calculateExceptionsSize(section, sectionViewModel.exceptions);
-}
 
 function createSegment(sectionSelectedSize: number, totalSize: number, sectionKey: string, sectionConfig: SectionConfig) {
   const config = sectionConfig[sectionKey];
@@ -56,7 +38,7 @@ export function calculateChartSegments<T extends Record<string, CleanerSection>>
       continue;
     }
 
-    const sectionSelectedSize = calculateSectionSelectedSize(section, sectionViewModel);
+    const sectionSelectedSize = calculateSectionSize({ section, sectionViewModel });
 
     if (sectionSelectedSize > 0) {
       segments.push(createSegment(sectionSelectedSize, totalSize, sectionKey, sectionConfig));
