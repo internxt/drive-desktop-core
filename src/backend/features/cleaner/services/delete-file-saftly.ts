ecd5fb8d4cd354ad7334444d6bd912db0f7a6031
Esteban Galvis <egalvis@shokworks.io>
2025-10-03 09:33:45 -0500

    Feat: implement cleanup actions (#31)

---
+++ src/backend/features/cleaner/services/delete-file-saftly.ts
@@ -0,0 +1,29 @@
+import { unlink } from 'node:fs/promises';
+
+import { logger } from '@/backend/core/logger/logger';
+import { FileSystemModule } from '@/backend/infra/file-system/file-system.module';
+import { AbsolutePath } from '@/backend/infra/file-system/file-system.types';
+
+import { cleanerStore } from '../stores/cleaner.store';
+
+type Props = {
+  absolutePath: AbsolutePath;
+};
+
+export async function deleteFileSafely({ absolutePath }: Props) {
+  try {
+    const { size } = await FileSystemModule.statThrow({ absolutePath });
+
+    await unlink(absolutePath);
+
+    cleanerStore.state.deletedFilesCount++;
+    cleanerStore.state.totalSpaceGained += size;
+  } catch (error) {
+    logger.warn({
+      tag: 'CLEANER',
+      msg: 'Failed to delete file, continuing with next file',
+      absolutePath,
+      error,
+    });
+  }
+}
