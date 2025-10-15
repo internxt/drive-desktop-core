875ff02a68632135af66ff8bde81b0c5d500ca83
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-15 11:30:41 +0200

    Update section-spinner.tsx

--- src/frontend/components/section-spinner.tsx
+++ src/frontend/components/section-spinner.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@phosphor-icons/react';
 
 export function SectionSpinner() {
   return (
-    <div className="flex h-[200px] w-full items-center justify-center">
+    <div className="flex w-full items-center justify-center" style={{ height: 200 }}>
       <Spinner className="animate-spin" />
     </div>
   );
