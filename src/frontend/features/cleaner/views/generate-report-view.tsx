5f1cb3dfcf1ffe66750cb816befc5d751c17de09
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-16 13:58:25 +0200

    Change cleaner title size (#44)

--- src/frontend/features/cleaner/views/generate-report-view.tsx
+++ src/frontend/features/cleaner/views/generate-report-view.tsx
@@ -10,16 +10,14 @@ export function GenerateReportView({ onGenerateReport, useTranslationContext }: 
   const { translate } = useTranslationContext();
 
   return (
-    <div className="flex flex-col items-center p-5" data-testid="generate-report-container">
-      <div className="flex flex-col items-center gap-4 text-center" data-testid="generate-report-content">
-        <div className="flex flex-col">
-          <p className="font-medium text-gray-100">{translate('settings.cleaner.generateReportView.title')}</p>
-          <p className="text-sm text-gray-80">{translate('settings.cleaner.generateReportView.description')}</p>
-        </div>
-        <Button onClick={onGenerateReport} className="w-max">
-          {translate('settings.cleaner.generateReportView.generateReport')}
-        </Button>
+    <div className="flex flex-col items-center justify-center gap-4 text-center" data-testid="generate-report-container">
+      <div className="flex flex-col gap-2">
+        <p className="text-xl font-semibold text-gray-100">{translate('settings.cleaner.generateReportView.title')}</p>
+        <p className="text-sm text-gray-80">{translate('settings.cleaner.generateReportView.description')}</p>
       </div>
+      <Button onClick={onGenerateReport} className="w-max">
+        {translate('settings.cleaner.generateReportView.generateReport')}
+      </Button>
     </div>
   );
 }
