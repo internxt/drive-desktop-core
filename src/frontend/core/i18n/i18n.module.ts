ce591b0827a3a4bc691e7ca820388705489d5387
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-20 17:55:23 +0200

    Commit

--- src/frontend/core/i18n/i18n.module.ts
+++ src/frontend/core/i18n/i18n.module.ts
@@ -1,4 +1,4 @@
 export { en } from './locales/en';
 export { es } from './locales/es';
 export { fr } from './locales/fr';
-export { TranslationFn, TranslationPath, TranslationArgs } from './i18n.types';
+export { Language, TranslationFn, TranslationPath, TranslationArgs } from './i18n.types';
