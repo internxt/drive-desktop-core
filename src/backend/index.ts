acc6ef08a8878f7321c0d73ebe934d11fdf5c742
Esteban Galvis <egalvis@shokworks.io>
2025-10-21 08:10:14 -0500

    Add BucketEntry class with a maximum size constant

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,7 @@
 export { logger, TLoggerBody } from './core/logger/logger';
 export { setupElectronLog } from './core/logger/setup-electron-log';
 export { throwWrapper } from './core/utils/throw-wrapper';
+export { BucketEntry } from './core/virtual-drive/bucket-entry';
 
 export { FileSystemModule, AbsolutePath, RelativePath } from './infra/file-system/file-system.module';
 export { PaymentsModule, UserAvailableProducts } from './features/payments/payments.module';
